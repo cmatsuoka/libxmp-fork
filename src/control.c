3d488c0c92a4151eb2971e5e0415dc3b8792c779
Claudio Matsuoka <cmatsuoka@gmail.com>
2021-06-22 19:46:54 -0300

    abi: use attribute symver instead of asm-based versioning
    LTO doesn't play well with asm-based symbol versioning, so use attribute
    __symver__ to use LTO in gcc 10.
    
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/control.c
+++ src/control.c
@@ -310,18 +310,14 @@ int xmp_channel_vol(xmp_context opaque, int chn, int vol)
 }
 
 #ifdef USE_VERSIONED_SYMBOLS
-LIBXMP_EXPORT extern int xmp_set_player_v40__(xmp_context, int, int);
+LIBXMP_EXPORT extern int xmp_set_player_v40__(xmp_context, int, int)
+                        __attribute__((__symver__("xmp_set_player@XMP_4.0")));
 LIBXMP_EXPORT extern int xmp_set_player_v41__(xmp_context, int, int)
-			__attribute__((alias("xmp_set_player_v40__")));
+			__attribute__((alias("xmp_set_player_v40__"), __symver__("xmp_set_player@XMP_4.1")));
 LIBXMP_EXPORT extern int xmp_set_player_v43__(xmp_context, int, int)
-			__attribute__((alias("xmp_set_player_v40__")));
+			__attribute__((alias("xmp_set_player_v40__"), __symver__("xmp_set_player@XMP_4.3")));
 LIBXMP_EXPORT extern int xmp_set_player_v44__(xmp_context, int, int)
-			__attribute__((alias("xmp_set_player_v40__")));
-
-asm(".symver xmp_set_player_v40__, xmp_set_player@XMP_4.0");
-asm(".symver xmp_set_player_v41__, xmp_set_player@XMP_4.1");
-asm(".symver xmp_set_player_v43__, xmp_set_player@XMP_4.3");
-asm(".symver xmp_set_player_v44__, xmp_set_player@@XMP_4.4");
+			__attribute__((alias("xmp_set_player_v40__"), __symver__("xmp_set_player@@XMP_4.4")));
 
 #define xmp_set_player__ xmp_set_player_v40__
 #else
@@ -428,21 +424,16 @@ int xmp_set_player__(xmp_context opaque, int parm, int val)
 }
 
 #ifdef USE_VERSIONED_SYMBOLS
-LIBXMP_EXPORT extern int xmp_get_player_v40__(xmp_context, int);
+LIBXMP_EXPORT extern int xmp_get_player_v40__(xmp_context, int)
+                __attribute__((__symver__("xmp_get_player@XMP_4.0")));
 LIBXMP_EXPORT extern int xmp_get_player_v41__(xmp_context, int)
-		__attribute__((alias("xmp_get_player_v40__")));
+		__attribute__((alias("xmp_get_player_v40__"), __symver__("xmp_get_player@XMP_4.1")));
 LIBXMP_EXPORT extern int xmp_get_player_v42__(xmp_context, int)
-		__attribute__((alias("xmp_get_player_v40__")));
+		__attribute__((alias("xmp_get_player_v40__"), __symver__("xmp_get_player@XMP_4.2")));
 LIBXMP_EXPORT extern int xmp_get_player_v43__(xmp_context, int)
-		__attribute__((alias("xmp_get_player_v40__")));
+		__attribute__((alias("xmp_get_player_v40__"), __symver__("xmp_get_player@XMP_4.3")));
 LIBXMP_EXPORT extern int xmp_get_player_v44__(xmp_context, int)
-		__attribute__((alias("xmp_get_player_v40__")));
-
-asm(".symver xmp_get_player_v40__, xmp_get_player@XMP_4.0");
-asm(".symver xmp_get_player_v41__, xmp_get_player@XMP_4.1");
-asm(".symver xmp_get_player_v42__, xmp_get_player@XMP_4.2");
-asm(".symver xmp_get_player_v43__, xmp_get_player@XMP_4.3");
-asm(".symver xmp_get_player_v44__, xmp_get_player@@XMP_4.4");
+		__attribute__((alias("xmp_get_player_v40__"), __symver__("xmp_get_player@@XMP_4.4")));
 
 #define xmp_get_player__ xmp_get_player_v40__
 #else
